434335a6a0d7af7fbb090d7495d319577c3ad27d
Daniel WItt <wi-dan@web.de>
2025-04-21 17:39:05 +0200

    Fixed Theme Setup. Added Header, Footer, Index.

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ define('ECHO_THEME_DIR', get_template_directory());
 define('ECHO_THEME_URI', get_template_directory_uri());
 
 // Load Composer-style autoloader
-require_once ECHO_THEME_DIR . '/inc/helpers/autoloader.php';
+require_once ECHO_THEME_DIR . '/inc/helpers/Autoloader.php';
 
 // Kick off the theme setup class
-if (class_exists('\Echo\Theme')) {
-    \Echo\Theme::get_instance();
+if (class_exists('\Echo\Classes\EchoTheme')) {  // Updated namespace
+    \Echo\Classes\EchoTheme::get_instance();   // Updated namespace
 }
--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-// Silence is golden.
+get_header();
+?>
+
+    <h1>Hello from index.php!</h1>
+
+<?php
+get_footer();
